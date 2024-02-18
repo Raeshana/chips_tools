7fbab725df2d9b949e26bb16feaa256d16cf32ae
Raeshana <rsookhoo@andrew.cmu.edu>
2024-02-18 03:53:19 -0500

    foundation for json file containing chip's challenge levels, started working on file to convert this json data to a dat file
    Unsure how to enter, read, and store data for upper_layer, lower_layer, and all optional fields

--- part_3_convert_json.py
+++ part_3_convert_json.py
@@ -1,6 +1,56 @@
 import cc_dat_utils
+import cc_classes
+import json
+
+def make_cc_level_pack_from_json(json_data):
+    # Initialize a new CCLevelPack
+    cc_level_pack = cc_classes.CCLevelPack()
+
+    ### Begin Add Code Here ###
+    levels = json_data["levels"]
+
+    # Loop through the json_data
+    for level in levels:
+        # Create a new Level object from the json_data by reading
+        new_level = cc_classes.CCLevel()
+
+        #  level_number
+        new_level.level_number = level["level_number"]
+        #print(new_level.level_number)
+
+        # time
+        new_level.time = level["time"]
+        #print(new_level.time)
+
+        # num_chips
+        new_level.num_chips = level["num_chips"]
+        #print(new_level.num_chips)
+
+        # upper_layer
+        new_level.upper_layer = level["upper_layer"]
+        print(new_level.upper_layer)
+
+        # lower_layer
+        new_level.lower_layer = level["lower_layer"]
+        print(new_level.lower_layer)
+
+        # optional_fields
+        
+
+        # Add that Game object to the game_library
+        cc_level_pack.add_level(new_level)
+    ### End Add Code Here ###
+
+    return cc_level_pack
 
 #Part 3
 #Load your custom JSON file
+input_json_file = "data/rsookhoo_cc1.json"
+
 #Convert JSON data to CCLevelPack
+reader = open(input_json_file, "r")
+json_data = json.load(reader)
+new_cc_level_pack = make_cc_level_pack_from_json(json_data)
+
 #Save converted data to DAT file
+#cc_dat_utils.write_cc_level_pack_to_dat(cc_dat, dat_file)
